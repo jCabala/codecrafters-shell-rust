58244fd383a151e44e7b54bd2c1ac201e3863b9d
jCabala <kuba.cabala6@gmail.com>
2026-05-27 09:30:40 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,16 @@ fn invalid_command_error(command: &str) {
 }
 
 fn main() {
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Read user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line");
-    input = input.trim().to_string();
+        // Read user input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+        input = input.trim().to_string();
 
-    // Throw an invalid command error
-    invalid_command_error(&input);
+        // Throw an invalid command error
+        invalid_command_error(&input);
+    }
 }
