1c2043bae0f266973293401c7674486e7b002f26
jCabala <kuba.cabala6@gmail.com>
2026-05-27 19:18:17 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -42,13 +42,16 @@ fn parse_args(input: &str) -> Vec<String> {
     let mut args = Vec::new();
     let mut current = String::new();
     let mut in_single_quote = false;
-    let mut chars = input.chars().peekable();
+    let mut in_double_quote = false;
+    let mut chars = input.chars();
 
     while let Some(c) = chars.next() {
         match c {
-            '\'' if !in_single_quote => in_single_quote = true,
+            '\'' if !in_single_quote && !in_double_quote => in_single_quote = true,
             '\'' if in_single_quote => in_single_quote = false,
-            ' ' | '\t' if !in_single_quote => {
+            '"' if !in_single_quote && !in_double_quote => in_double_quote = true,
+            '"' if in_double_quote => in_double_quote = false,
+            ' ' | '\t' if !in_single_quote && !in_double_quote => {
                 if !current.is_empty() {
                     args.push(current.clone());
                     current.clear();
