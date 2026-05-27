3e974b19cd5d9693fdd23372262d4fe2b965b528
jCabala <kuba.cabala6@gmail.com>
2026-05-27 09:29:17 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
+
+fn invalid_command_error(command: &str) {
+    eprintln!("{command}: command not found");
+}
+
 fn main() {
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    // Read user input
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    input = input.trim().to_string();
+
+    // Throw an invalid command error
+    invalid_command_error(&input);
 }
